26496e8645394698f87f070fc1ce2a3927da03e1
istrugatskiy <73840560+istrugatskiy@users.noreply.github.com>
2023-05-27 00:39:09 -0400

    improve router tests

--- packages/mamk-router/__tests__/index.test.ts
+++ packages/mamk-router/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { redirect } from '..';
+import { redirect, start_router, INTERNAL_FOR_TESTING } from '..';
 
 describe('redirect', () => {
     it('should redirect to the given path', () => {
@@ -13,5 +13,34 @@ describe('redirect', () => {
         const spy = jest.spyOn(window.history, 'pushState');
         redirect(path);
         expect(spy).toHaveBeenCalledWith({ index: 2 }, '', path);
+    });
+});
+
+describe('start_router', () => {
+    it('should start the router', () => {
+        const spy = jest.spyOn(window, 'addEventListener');
+        const create_fake_path = (path: string) => {
+            return {
+                path: `/${path}`,
+                component: `${path}-page`,
+                title: `${path} page`,
+                require_auth: false,
+                special_path: false,
+                show_user: false,
+                load: () => Promise.resolve(),
+            };
+        };
+        const test_route_list: route_list = {
+            no_auth: create_fake_path('no-auth'),
+            not_found: create_fake_path('not-found'),
+            layout: {
+                '/': create_fake_path('home'),
+                '/test': create_fake_path('test'),
+            },
+            $outlet: document.createElement('div'),
+        };
+        start_router(test_route_list);
+        expect(INTERNAL_FOR_TESTING.state.routes).toEqual(test_route_list);
+        expect(spy).toHaveBeenCalledWith('popstate', expect.any(Function), false);
     });
 });
